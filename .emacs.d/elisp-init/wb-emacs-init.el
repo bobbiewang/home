a7003dcabb740328aec1216d64f313e0296d5abf
Bo Wang <Bo.Wang@Office>
2009-01-13 14:14:14 +0800

    增加函数 wb-count-words。

--- .emacs.d/elisp-init/wb-emacs-init.el
+++ .emacs.d/elisp-init/wb-emacs-init.el
@@ -378,6 +378,13 @@ when a region-mark is active."
   (interactive)
   (insert (format-time-string "%H:%M:%S")))
 
+(defun wb-count-words (&optional begin end)
+  "count words between BEGIN and END (region); if no region defined, count words in buffer"
+  (interactive "r")
+  (let ((b (if mark-active begin (point-min)))
+      (e (if mark-active end (point-max))))
+    (message "Word count: %s" (how-many "\\w+" b e))))
+
 ;; 自动换行显示
 (setq truncate-lines nil)
 (setq truncate-partial-width-windows nil)
