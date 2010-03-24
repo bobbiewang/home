7553d41969af153b0c5c6da838cd9fe431ef41d3
Bo Wang <Bo.Wang@Home>
2010-03-24 23:35:32 +0800

    移动 yasnippet 的设置到 auto-complete 和 company 的前面。

--- .emacs.d/elisp-init/wb-emacs-init.el
+++ .emacs.d/elisp-init/wb-emacs-init.el
@@ -1720,6 +1720,19 @@ directory, select directory. Lastly the file is opened."
 
 (robust-require snippet)
 
+(robust-require yasnippet
+  (yas/initialize)
+  ;; yas/root-directory 既可以设置为一个目录，也可以设置为一个列表。如
+  ;; 果设置为列表，第一个目录用于开发个人的 snippet，相关命令（如
+  ;; yas/new-snippet）将在这个目录下创建新的 snippet
+  (setq yas/root-directory
+        (list "~/.emacs.d/elisp-personal/yasnippets" ; 自定义的 snippet
+              (concat                                ; 随 yasnippet 发布的 snippet
+               (file-name-directory (locate-library "yasnippet"))
+               "snippets")))
+  ;; yas/load-directory 只支持 string 参数，借助 mapc 作用于 list
+  (mapc 'yas/load-directory yas/root-directory))
+
 (robust-require auto-complete-config
   ;; 使用 auto-complete 自带的 dict
   (add-to-list 'ac-dictionary-directories
@@ -2347,20 +2360,6 @@ the length of the whitespace"
   (add-to-list 'auto-mode-alist '("\.html\.erb$". rhtml-mode))
   (add-hook 'rhtml-mode-hook
             (lambda () (rinari-launch))))
-
-;; Rails Snippets
-(robust-require yasnippet
-  (yas/initialize)
-  ;; yas/root-directory 既可以设置为一个目录，也可以设置为一个列表。如
-  ;; 果设置为列表，第一个目录用于开发个人的 snippet，相关命令（如
-  ;; yas/new-snippet）将在这个目录下创建新的 snippet
-  (setq yas/root-directory
-        (list "~/.emacs.d/elisp-personal/yasnippets" ; 自定义的 snippet
-              (concat                                ; 随 yasnippet 发布的 snippet
-               (file-name-directory (locate-library "yasnippet"))
-               "snippets")))
-  ;; yas/load-directory 只支持 string 参数，借助 mapc 作用于 list
-  (mapc 'yas/load-directory yas/root-directory))
 
 ;; YAML 支持
 (robust-require yaml-mode
