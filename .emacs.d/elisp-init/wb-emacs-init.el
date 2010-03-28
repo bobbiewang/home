7a162b0cb2e0ffde077a3cf92299e6fd7a023a79
Bo Wang <Bo.Wang@Home>
2010-03-28 21:05:58 +0800

    增加命令 wb-swap-windows。

--- .emacs.d/elisp-init/wb-emacs-init.el
+++ .emacs.d/elisp-init/wb-emacs-init.el
@@ -479,6 +479,25 @@ configuration before the split."
         (when first (other-window 1)))
     (message "There are not exactly 2 windows.")))
 
+;; 如果当前 frame 只有两个 windows，交换 windows
+(defun wb-swap-windows ()
+  "If you have 2 windows, it swaps them."
+  (interactive)
+  (cond ((/= (count-windows) 2)
+         (message "You need exactly 2 windows to do this."))
+        (t
+         (let* ((w1 (first (window-list)))
+                (w2 (second (window-list)))
+                (b1 (window-buffer w1))
+                (b2 (window-buffer w2))
+                (s1 (window-start w1))
+                (s2 (window-start w2)))
+           (set-window-buffer w1 b2)
+           (set-window-buffer w2 b1)
+           (set-window-start w1 s2)
+           (set-window-start w2 s1))))
+  (other-window 1))
+
 ;;; Frame
 
 ;; MS Windows 平台 frame 控制
