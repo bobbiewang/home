d01604bbf7058eb242af7d82dba212e7d12cd3d4
Bo Wang <Bo.Wang@Office>
2011-04-25 16:06:28 +0800

    增加 yaml-mode 的 autoload。

--- .emacs.d/elisp-init/wb-emacs-init.el
+++ .emacs.d/elisp-init/wb-emacs-init.el
@@ -2904,6 +2904,8 @@ cursor to the new line."
 
 ;; YAML 支持
 (with-library "yaml-mode"
+  (autoload 'yaml-mode "yaml-mode" "yaml mode")
+  
   (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
   (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode))
 
