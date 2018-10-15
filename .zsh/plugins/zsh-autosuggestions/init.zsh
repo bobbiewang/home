1b3463763e47c9af85db72f3f538b6df59824b38
Bo Wang <Bobbie.Wang@gmail.com>
2018-10-16 00:13:23 +0800

    删除现有 submodule，需要重新组织。

--- .zsh/plugins/zsh-autosuggestions/init.zsh
+++
@@ -1,2 +0,0 @@
-local init_file=~/.zsh/plugins/zsh-autosuggestions/external/zsh-autosuggestions.zsh
-[ -f $init_file ] && source $init_file
--- .zsh/plugins/zsh-history-substring-search/init.zsh
+++
@@ -1,2 +0,0 @@
-local init_file=~/.zsh/plugins/zsh-history-substring-search/external/zsh-history-substring-search.zsh
-[ -f $init_file ] && source $init_file
